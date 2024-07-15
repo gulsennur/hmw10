18d6fcad063fc19040617411d272bbe4a8531262
gulsennur <gulsennur1995@gmail.com>
2024-07-15 13:21:52 +0300

    added.

--- src/main/java/org/example/generic/GenericClass.java
+++ src/main/java/org/example/generic/GenericClass.java
@@ -1,2 +1,22 @@
-package org.example.generic;public class GenericClass {
+package org.example.generic;
+
+ public class GenericClass<T> {
+    private T value;
+
+    public GenericClass(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void displayValue() {
+        System.out.println("Value: " + value);
+    }
 }
+
--- src/main/java/org/example/generic/Stream.java
+++ src/main/java/org/example/generic/Stream.java
@@ -1,2 +1,49 @@
-package org.example.generic;public class Stream {
+package org.example.generic;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.Scanner;
+public class Stream {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // get length of array
+        System.out.print("Dizinin uzunluğunu girin: ");
+        int length = scanner.nextInt();
+
+        // create array
+        int[] array = new int[length];
+
+        // get element of array
+        System.out.println("Dizinin elemanlarını girin:");
+        for (int i = 0; i < length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        // enter searching of array
+        System.out.print("Aranacak öğeyi girin: ");
+        int target = scanner.nextInt();
+
+        // searching element and print the result
+        int result = searchElementWithStream(array, target);
+        if (result == -1) {
+            System.out.println("Öğe dizide bulunamadı.");
+        } else {
+            System.out.println("Öğe bulundu: " + result);
+        }
+
+        scanner.close();
+    }
+
+    //  Method that searches for the item in the array.
+    public static int searchElementWithStream(int[] array, int target) {
+        //Search for the item using the Stream API
+        OptionalInt foundElement = Arrays.stream(array)
+                .filter(x -> x == target)
+                .findFirst();
+
+        // Return the result, depending on whether the item was found
+        return foundElement.orElse(-1);
+    }
 }
+
